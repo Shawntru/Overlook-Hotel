aef4644390db77c23c713413d5d2bf19bad88566
Shawn Truesdale <shawnctruesdale@gmail.com>
2020-11-03 18:33:13 -0700

    Add async functions for data retrieval on page load

--- src/index.js
+++ src/index.js
@@ -7,6 +7,24 @@ import './images/turing-logo.png'
 import { fetchData } from './fetch.js';
 import { hotel } from './hotel';
 
-window.onload = () => {
-  hotel.getHotelData();
-};
+window.onload = initializeSite();
+
+async function initializeSite() {
+  await hotel.getHotelData();
+  let userList = await fetchData('users');
+  getLoginInfo();
+}
+
+function getLoginInfo() {
+  // Query DOM elements for user input, check password
+  // Will use temp username and pass set below:
+  const username = 'customer10';
+  const password = 'overlook2020';
+  const userId = parseInt(username.slice(8,10));
+  if (1 > userId > 50) alert('Need valid username');
+  loginUser(userId);
+}
+
+function loginUser(userId) {
+  console.log('Logged in as user' + userId);
+}
